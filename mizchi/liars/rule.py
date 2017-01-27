22e2160522868d86c0035ef0d1f5a518d7aec4ce
arcMizchi <wtnb0sp@gmail.com>
2017-01-27 21:59:43 +0900

    test

---
+++ mizchi/liars/rule.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class GameMaster():
+	def __init__(self, player=4):
+		self.symbols = ['h', 'c', 'd', 's']
+		self.number = 13
+		self.cardset = []
+
+		perm = np.random.permutation(player)
+		for i in range(0, player):
+			for j in range(0, self.number):
+				self.cardset.append([self.symbols[perm[i]], j])
+
+	def refer_log(self, field):
+		self.log = field.log
+		return self.log[-1,-1]
+
+	def 
